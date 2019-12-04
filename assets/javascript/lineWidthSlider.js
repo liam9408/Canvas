7b4dc3d9a0c9aa5afc697bd0ddff574bb5b77af8
liam9408 <57433615+liam9408@users.noreply.github.com>
2019-12-04 21:15:28 +0800

    fixed position of lineWidth Slider

--- assets/javascript/lineWidthSlider.js
+++ assets/javascript/lineWidthSlider.js
@@ -8,4 +8,24 @@ slider.oninput = function() {
   value = this.value;
   lineWidth = value;
   console.log(value)
+  $('#linewidthIndicator').html(`LW:<br>${lineWidth}px`);
 }
+
+
+
+
+//
+// PICKER -------------------------
+//
+
+  $('#lineWidth-picker').mouseover(function() {
+    $('#lineWidth-picker').animate({height: "200px", width: "50px"}, 300)
+    $('#range').slideDown()
+    $('#linewidthIndicator').css('display', 'none');
+  })
+  
+  $('#lineWidth-picker').mouseleave(function() {
+    $('#lineWidth-picker').animate({height: "50px", width: "50px"}, 300)
+    $('#range').slideUp()
+    $('#linewidthIndicator').css('display', '');
+  })
